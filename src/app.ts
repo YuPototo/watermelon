3981ee893fd54eed794619fce2c0426356c66983
Yu Qin <sirius.qinyu@gmail.com>
2022-02-01 20:03:37 +0800

    add cors and helmet

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response, NextFunction } from 'express'
 import type { Express } from 'express-serve-static-core'
+import cors from 'cors'
+import helmet from 'helmet'
+
 import logger, { useLog } from '@/utils/logger'
 
 import userRouter from './routes/userRoute'
@@ -12,8 +15,11 @@ const API_PREFIX = '/api'
 export async function createApp(): Promise<Express> {
     const app = express()
 
+    // middleware
+    app.enable('trust proxy') // 使用 nginx proxy 时要用
+    app.use(helmet()) // 若干开箱即用的安全措施
+    app.use(cors()) // 允许跨域访问
     app.use(express.json()) // 保证 http request body 会被作为 json 传入
-
     useLog(app)
 
     // routes
